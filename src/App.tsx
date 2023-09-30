659963b9d7d9f0f7946682864c8a545bb3ceddce
Dam Hong Duc <hongduc001h@gmail.com>
2023-09-30 17:33:20 +0700

    config husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Home from 'screens/home/Home';
-import { View } from 'react-native';
+import React from "react";
+import Home from "@screens/home/Home";
+import { View } from "react-native";
 
 const App = () => {
-  return (
-    <View>
-      <Home />
-    </View>
-  );
+	return (
+		<View>
+			<Home />
+		</View>
+	);
 };
 
 export default App;
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
-  );
+	return (
+		<View>
+			<Text>Home Screen</Text>
+		</View>
+	);
 };
 
 export default Home;
